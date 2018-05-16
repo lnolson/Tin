abcbd98ce5ae5babfbb82aae6168f06cf392db27
Loren Olson <loren.olson@asu.edu>
2018-05-16 15:03:01 -0700

    ImageBench is a very small project used during recent updated to TImage.

---
+++ examples/ImageBench/ImageBench/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  ImageBench
+//
+//  Created by Loren Olson on 4/2/18.
+//  Copyright © 2018 ASU. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
+
+// This empty class is here to fix an Xcode 9.1/9.2 bug.
+// Use FooBarWindow in Main.storyboard to silence bogus error.
+// Reportedly fixed in next Xcode release. See http://www.openradar.me/35511761
+class FooBarWindow: NSWindow {
+    
+}
+
---
+++ examples/ImageBench/ImageBench/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ImageBench
+//
+//  Created by Loren Olson on 4/2/18.
+//  Copyright © 2018 ASU. All rights reserved.
+//
+
+import Cocoa
+import Tin
+
+
+class ViewController: TController {
+
+    var scene: Scene!
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        view.window?.title = "Image"
+        makeView(width: 1000.0, height: 600.0)
+        scene = Scene()
+        present(scene: scene)
+        scene.view?.showStats = true
+    }
+
+
+}
+
+
+class Scene: TScene {
+    
+    var photo: TImage!
+    
+    override func setup() {
+        photo = TImage(contentsOfFileInBundle: "henge.jpg")
+    }
+    
+    override func update() {
+        background(gray: 0.5)
+        
+        let x = remap(value: tin.mouseX, start1: 0, stop1: tin.width, start2: -500, stop2: 0)
+        image(image: photo, x: x, y: 0)
+    }
+}
+
